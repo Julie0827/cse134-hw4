1b2d184171fbf0c034438caac6282fa256eb7b1b
Julie827 <jackjack170827@gmail.com>
2025-02-24 07:30:43 -0800

    .

--- scripts/search.js
+++ scripts/search.js
@@ -24,6 +24,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     async function handleSearchSidebar() {
         resultsContainer.innerHTML = "";
+        console.log(resultsContainer.children.length === 0);
 
         let originalQuery = searchBox.value.trim();
         let query = originalQuery.toLowerCase();
