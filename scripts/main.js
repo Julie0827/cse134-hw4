8ccda629bbde93bcd5703cb1e77698cbd70d4bbb
Julie827 <jackjack170827@gmail.com>
2025-02-27 16:32:12 -0800

    .

--- scripts/main.js
+++ scripts/main.js
@@ -4,14 +4,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
     dropdownNavBtn.addEventListener("click", function() {
         if (dropdownNav.classList.contains("hide")) {
-            dropdownNav.classList.remove("hide");
             dropdownNavBtn.src = "assets/images/x.svg";
             dropdownNavBtn.alt = "Close navigation menu";
+            dropdownNav.classList.remove("hide");
 
         } else {
-            dropdownNav.classList.add("hide");
             dropdownNavBtn.src = "assets/images/hamburger.svg";
             dropdownNavBtn.alt = "Open navigation menu";
+            dropdownNav.classList.add("hide");
         }
     });
 });
