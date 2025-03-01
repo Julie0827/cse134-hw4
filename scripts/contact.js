fd090cbf4c98bb796dfea0a0c14eb1af5b741164
Julie827 <jackjack170827@gmail.com>
2025-03-01 08:47:16 -0800

    .

--- scripts/contact.js
+++ scripts/contact.js
@@ -9,4 +9,45 @@ document.addEventListener("DOMContentLoaded", function () {
     contactForm.addEventListener("input", function () {
         botField.value = "false";
     });
+
+    const regexAllowed = {
+        name: /^[A-Za-zÀ-ÖØ-öø-ÿ' -]+$/,
+        email: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~@-]+$/,
+        comments: /^[\w\sÀ-ÖØ-öø-ÿ.,!?'"^*+/=(){|}\[\]\\<>:;~&@#%\$-]+$/
+    }
+
+    let flashTimeout;
+
+    function handleIllegalChar(e) {
+        const input = e.target;
+        const fieldName = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+        const rx = regexAllowed[input.id];
+        const errorOutput = document.getElementById(`${input.id}-char-error`);
+
+        if (input.value && !rx.test(input.value)) {
+            console.log("invalid");
+            const illegalChar = [...input.value].find(char => !new RegExp(rx).test(char));
+            
+            errorOutput.textContent = `Illegal character "${illegalChar}" detected in ${fieldName} field.`;
+            errorOutput.classList.remove("hidden");
+
+            input.classList.remove("flash");
+            void input.offsetWidth;
+
+            input.classList.add("flash");
+
+            input.value = input.value.replace(illegalChar, "");
+
+            clearTimeout(flashTimeout);
+
+            flashTimeout = setTimeout(() => {
+                errorOutput.classList.add("hidden");
+                input.classList.remove("flash");
+            }, 2000);
+        }
+    }
+
+    document.getElementById("name").addEventListener("input", handleIllegalChar);
+    document.getElementById("email").addEventListener("input", handleIllegalChar);
+    document.getElementById("comments").addEventListener("input", handleIllegalChar);
 });
